3c8e70c472291810680ead14c2daa2bd2b241e25
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-05-05 19:02:36 +0200

    Implement edge detection by simplifying colors

--- pyassetto.py
+++ pyassetto.py
@@ -2,12 +2,22 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 
+
+def process_image(original_image):
+    processed_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_image = cv2.Canny(
+        processed_image, threshold1=200, threshold2=300
+    )  # tweakable
+    return processed_image
+
+
 # Use PIL ImageGrab module to grab the printscreen frames
 while True:
     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-    # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype="uint8")
+    new_screen = process_image(screen)
 
-    cv2.imshow("window", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    cv2.imshow("window", new_screen)
+    # cv2.imshow("window", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 
     # close screen grab with 'q'
     if cv2.waitKey(25) & 0xFF == ord("q"):
