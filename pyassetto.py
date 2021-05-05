da0cac6802efe8ede62c1320934b5169e815631d
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-05-05 18:51:41 +0200

    Fix screen grab colors

--- pyassetto.py
+++ pyassetto.py
@@ -4,10 +4,10 @@ import cv2
 
 # Use PIL ImageGrab module to grab the printscreen frames
 while True:
-    printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
+    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
     # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype="uint8")
 
-    cv2.imshow("window", np.array(printscreen_pil))
+    cv2.imshow("window", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 
     # close screen grab with 'q'
     if cv2.waitKey(25) & 0xFF == ord("q"):
