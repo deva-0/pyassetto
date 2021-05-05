36def73d2f36a90b78daa86b27415d74f2eaaaac
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-05-05 20:37:22 +0200

    Add a way to send input to the game

--- pyassetto.py
+++ pyassetto.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
+from directkeys import PressKey, ReleaseKey, UpArrow, LeftArrow, RightArrow, DownArrow
 
 
 def process_image(original_image):
@@ -11,10 +13,21 @@ def process_image(original_image):
     return processed_image
 
 
+for i in list(range(4))[::-1]:
+    print(i + 1)
+    time.sleep(1)
+
+
 # Use PIL ImageGrab module to grab the printscreen frames
 while True:
     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
     new_screen = process_image(screen)
+
+    # print("down")
+    # PressKey(UpArrow)
+    # time.sleep(3)
+    # print("up")
+    # ReleaseKey(UpArrow)
 
     cv2.imshow("window", new_screen)
     # cv2.imshow("window", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
