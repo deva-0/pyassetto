19dd39977b6af6f8ac815d8146f63c35f29a8b6c
Adrian Zaręba <dev.adrianzareba@gmail.com>
2021-05-06 15:33:45 +0200

    Major code clean-up

--- pyassetto.py
+++ pyassetto.py
@@ -1,136 +1,16 @@
 import numpy as np
-from PIL import ImageGrab
 import cv2
 import time
-from numpy import ones, vstack
-from numpy.linalg import lstsq
-from directkeys import PressKey, ReleaseKey, UpArrow, DownArrow, LeftArrow, RightArrow
-from statistics import mean
-
-
-def straight():
-    PressKey(UpArrow)
-    ReleaseKey(LeftArrow)
-    ReleaseKey(RightArrow)
-
-
-def left():
-    PressKey(LeftArrow)
-    ReleaseKey(UpArrow)
-    ReleaseKey(RightArrow)
-    ReleaseKey(LeftArrow)
-
-
-def right():
-    PressKey(RightArrow)
-    ReleaseKey(LeftArrow)
-    ReleaseKey(UpArrow)
-    ReleaseKey(RightArrow)
-
-
-def slow_ya_roll():
-    ReleaseKey(UpArrow)
-    ReleaseKey(LeftArrow)
-    ReleaseKey(RightArrow)
+from directkeys import PressKey, ReleaseKey, UpArrow, LeftArrow, DownArrow, RightArrow
+from draw_lanes import draw_lanes
+from grabscreen import grab_screen
 
 
 def roi(img, vertices):
-
     mask = np.zeros_like(img)
-
     cv2.fillPoly(mask, vertices, 255)
-
     masked = cv2.bitwise_and(img, mask)
     return masked
-
-
-def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
-    try:
-        # finds the maximum y value for a lane marker
-        ys = []
-        for i in lines:
-            for ii in i:
-                ys += [ii[1], ii[3]]
-        min_y = min(ys)
-        max_y = 600
-        new_lines = []
-        line_dict = {}
-
-        for idx, i in enumerate(lines):
-            for xyxy in i:
-                x_coords = (xyxy[0], xyxy[2])
-                y_coords = (xyxy[1], xyxy[3])
-                A = vstack([x_coords, ones(len(x_coords))]).T
-                m, b = lstsq(A, y_coords)[0]
-
-                x1 = (min_y - b) / m
-                x2 = (max_y - b) / m
-
-                line_dict[idx] = [m, b, [int(x1), min_y, int(x2), max_y]]
-                new_lines.append([int(x1), min_y, int(x2), max_y])
-
-        final_lanes = {}
-
-        for idx in line_dict:
-            final_lanes_copy = final_lanes.copy()
-            m = line_dict[idx][0]
-            b = line_dict[idx][1]
-            line = line_dict[idx][2]
-
-            if len(final_lanes) == 0:
-                final_lanes[m] = [[m, b, line]]
-
-            else:
-                found_copy = False
-
-                for other_ms in final_lanes_copy:
-
-                    if not found_copy:
-                        if abs(other_ms * 1.2) > abs(m) > abs(other_ms * 0.8):
-                            if (
-                                abs(final_lanes_copy[other_ms][0][1] * 1.2)
-                                > abs(b)
-                                > abs(final_lanes_copy[other_ms][0][1] * 0.8)
-                            ):
-                                final_lanes[other_ms].append([m, b, line])
-                                found_copy = True
-                                break
-                        else:
-                            final_lanes[m] = [[m, b, line]]
-
-        line_counter = {}
-
-        for lanes in final_lanes:
-            line_counter[lanes] = len(final_lanes[lanes])
-
-        top_lanes = sorted(line_counter.items(), key=lambda item: item[1])[::-1][:2]
-
-        lane1_id = top_lanes[0][0]
-        lane2_id = top_lanes[1][0]
-
-        def average_lane(lane_data):
-            x1s = []
-            y1s = []
-            x2s = []
-            y2s = []
-            for data in lane_data:
-                x1s.append(data[2][0])
-                y1s.append(data[2][1])
-                x2s.append(data[2][2])
-                y2s.append(data[2][3])
-            return int(mean(x1s)), int(mean(y1s)), int(mean(x2s)), int(mean(y2s))
-
-        l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
-        l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
-
-        return (
-            [l1_x1, l1_y1, l1_x2, l1_y2],
-            [l2_x1, l2_y1, l2_x2, l2_y2],
-            lane1_id,
-            lane2_id,
-        )
-    except Exception as e:
-        print(str(e))
 
 
 def process_img(image):
@@ -156,7 +36,7 @@ def process_img(image):
 
     processed_img = roi(processed_img, [vertices])
 
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi / 180, 180, 500, 30)
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi / 180, 180, 20, 15)
     m1 = 0
     m2 = 0
     try:
@@ -186,12 +66,43 @@ def process_img(image):
     return processed_img, original_image, m1, m2
 
 
+def straight():
+    PressKey(UpArrow)
+    ReleaseKey(LeftArrow)
+    ReleaseKey(RightArrow)
+
+
+def left():
+    PressKey(LeftArrow)
+    ReleaseKey(UpArrow)
+    ReleaseKey(RightArrow)
+    ReleaseKey(LeftArrow)
+
+
+def right():
+    PressKey(RightArrow)
+    ReleaseKey(LeftArrow)
+    ReleaseKey(UpArrow)
+    ReleaseKey(RightArrow)
+
+
+def slow():
+    ReleaseKey(UpArrow)
+    ReleaseKey(LeftArrow)
+    ReleaseKey(RightArrow)
+
+
+for i in list(range(4))[::-1]:
+    print(i + 1)
+    time.sleep(1)
+
+
 def main():
     while True:
-        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+        screen = grab_screen(region=(0, 40, 800, 640))
         new_screen, original_image, m1, m2 = process_img(screen)
-        cv2.imshow("window", new_screen)
-        # cv2.imshow("window2", cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+        # cv2.imshow('window', new_screen)
+        cv2.imshow("window2", cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 
         if m1 < 0 and m2 < 0:
             right()
